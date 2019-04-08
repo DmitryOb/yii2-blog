dc065746ff0c35e246ba51d56f554fa7cecaea85
DmitryOb <breakdance101@yandex.ru>
2019-04-09 00:44:17 +0700

    Сокрытие админки

--- modules/admin/Module.php
+++ modules/admin/Module.php
@@ -2,24 +2,40 @@
 
 namespace app\modules\admin;
 
-/**
- * admin module definition class
- */
+
+use Yii;
+use yii\filters\AccessControl;
+
 class Module extends \yii\base\Module
 {
-    /**
-     * {@inheritdoc}
-     */
     public $layout = '/admin';
     public $controllerNamespace = 'app\modules\admin\controllers';
 
-    /**
-     * {@inheritdoc}
-     */
-    public function init()
+    public function behaviors()
+	{
+		return [
+			'access' => [
+				'class' => AccessControl::className(),
+				'denyCallback' => function($rule, $action)
+				{
+					throw new \yii\web\NotFoundHttpException();
+				},
+				'rules' => [
+					[
+						'allow' => true,
+						'matchCallback' => function($rule, $action)
+						{
+							return Yii::$app->user->identity->isAdmin;
+						}
+					]
+				],
+			],
+		];
+	}
+
+	public function init()
     {
         parent::init();
-
         // custom initialization code goes here
     }
 }
